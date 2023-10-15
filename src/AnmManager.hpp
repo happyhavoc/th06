b1a7837553ca346b2c2d1937e63d1d4538637f4e
roblabla <unfiltered@roblab.la>
2023-10-15 22:38:43 +0200

    Add AnmVm contents

--- src/AnmManager.hpp
+++ src/AnmManager.hpp
@@ -7,18 +7,79 @@
 
 struct AnmLoadedSprite
 {
+    u32 sourceFileIndex;
+    D3DXVECTOR2 startPixelInclusive;
+    D3DXVECTOR2 endPixelInclusive;
+    f32 textureHeight;
+    f32 textureWidth;
+    D3DXVECTOR2 uvStart;
+    D3DXVECTOR2 uvEnd;
+    f32 heightPx;
+    f32 widthPx;
+    i32 spriteId;
+};
+
+struct AnmTimer
+{
+    i32 previous;
+    f32 subFrame;
+    i32 current;
+};
+
+struct AnmRawInstr
+{
 };
 struct AnmVm
 {
-};
-struct AnmRawInstr
-{
+    D3DXVECTOR3 rotation;
+    D3DXVECTOR3 angleVel;
+    f32 scaleY;
+    f32 scaleX;
+    f32 scaleInterpFinalY;
+    f32 scaleInterpFinalX;
+    D3DXVECTOR2 uvScrollPos;
+    AnmTimer currentTimeInScript;
+    D3DMATRIX matrix;
+    D3DCOLOR color;
+    u32 flags;
+    u16 alphaInterpEndTime;
+    u16 scaleInterpEndTime;
+    u16 autoRotate;
+    i16 pendingInterrupt;
+    u16 posInterpEndTime;
+    // Two padding bytes
+    D3DXVECTOR3 pos;
+    f32 scaleInterpInitialY;
+    f32 scaleInterpInitialX;
+    AnmTimer scaleInterpTime;
+    i16 spriteNumber;
+    i16 anotherSpriteNumber;
+    i16 anmFileIndex;
+    // Two padding bytes
+    AnmRawInstr *beginingOfScript;
+    AnmRawInstr *currentInstruction;
+    AnmLoadedSprite *sprite;
+    D3DCOLOR alphaInterpInitial;
+    D3DCOLOR alphaInterpFinal;
+    D3DXVECTOR3 posInterpInitial;
+    D3DXVECTOR3 posInterpFinal;
+    D3DXVECTOR3 pos2;
+    AnmTimer posInterpTime;
+    i32 timeOfLastSpriteSet;
+    AnmTimer alphaInterpTime;
+    u8 fontWidth;
+    u8 fontHeight;
+    // Two final padding bytes
 };
 struct AnmRawEntry
 {
 };
+
 struct RenderVertexInfo
 {
+    D3DXVECTOR3 position;
+    D3DCOLOR diffuseColor;
+    D3DXVECTOR2 textureUV;
 };
 
 struct AnmManager
--- src/inttypes.hpp
+++ src/inttypes.hpp
@@ -6,3 +6,4 @@ typedef short i16;
 typedef unsigned short u16;
 typedef int i32;
 typedef unsigned int u32;
+typedef float f32;
