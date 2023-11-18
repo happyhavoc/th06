d6dd7c59d9aa28dc471ce3e649a6fb07b1e4d998
roblabla <unfiltered@roblab.la>
2023-11-18 20:06:28 +0100

    Add cygwin to the PATH

--- scripts/th06vars.bat
+++ scripts/th06vars.bat
@@ -4,8 +4,9 @@
 @SET MS_VC_PATH=%VS_BASE_PATH%\VC7
 @SET DXSDK_DIR=%DEVENV_PREFIX%\mssdk
 @SET PY_PATH=%DEVENV_PREFIX%\python
+@SET CYGWIN_PATH=%DEVENV_PREFIX%\cygwin\bin
 
-@SET PATH=%PY_PATH%;%MS_VC_PATH%\Bin;%VS_BASE_PATH%\FrameworkSDK\Bin;%VS_BASE_PATH%\Common7\IDE;%PATH%
+@SET PATH=%PY_PATH%;%MS_VC_PATH%\Bin;%VS_BASE_PATH%\FrameworkSDK\Bin;%VS_BASE_PATH%\Common7\IDE;%CYGWIN_PATH%;%PATH%
 @SET INCLUDE=%DXSDK_DIR%\include;%MS_VC_PATH%\INCLUDE;%MS_VC_PATH%\PlatformSDK\include\prerelease;%MS_VC_PATH%\PlatformSDK\include;%MS_VC_PATH%\PlatformSDK\common\include\prerelease;%MS_VC_PATH%\PlatformSDK\common\include
 @SET LIB=%DXSDK_DIR%\lib;%MS_VC_PATH%\LIB;%MS_VC_PATH%\PlatformSDK\lib\prerelease;%MS_VC_PATH%\PlatformSDK\lib;%MS_VC_PATH%\PlatformSDK\common\lib\prerelease;%MS_VC_PATH%\PlatformSDK\common\lib;%MS_VC_PATH%\PlatformSDK\x86\lib
 
