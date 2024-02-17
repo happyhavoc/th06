d45efa23aaf4bf81313bc2d3a80f44770c029d53
Mary Guillemard <mary@mary.zone>
2024-02-17 19:22:10 +0100

    Address comments
    Signed-off-by: Mary Guillemard <mary@mary.zone>

--- src/Rng.cpp
+++ src/Rng.cpp
@@ -2,21 +2,21 @@
 
 DIFFABLE_STATIC(Rng, g_Rng);
 
-unsigned short Rng::GetRandomU16(void)
+u16 Rng::GetRandomU16(void)
 {
-    unsigned short a = (this->seed ^ 0x9630) - 0x6553;
+    u16 a = (this->seed ^ 0x9630) - 0x6553;
 
     this->seed = (((a & 0xc000) >> 14) + a * 4) & 0xFFFF;
     this->generationCount++;
     return this->seed;
 }
 
-unsigned int Rng::GetRandomU32(void)
+u32 Rng::GetRandomU32(void)
 {
     return GetRandomU16() << 16 | GetRandomU16();
 }
 
-float Rng::GetRandomF32ZeroToOne(void)
+f32 Rng::GetRandomF32ZeroToOne(void)
 {
     return (float)GetRandomU32() / (float)0xFFFFFFFF;
 }
