b1765bd2925fccea17b792bcf0f6909876331419
roblabla <unfiltered@roblab.la>
2024-02-17 22:10:56 +0100

    Add new build function in build.py script

--- scripts/build.py
+++ scripts/build.py
@@ -7,6 +7,18 @@ from winhelpers import run_windows_program
 SCRIPTS_DIR = Path(__file__).parent
 
 
+def build(build_type):
+    configure(build_type)
+
+    # Then, run the build. We use run_windows_program to automatically go through
+    # wine if running on linux/macos. scripts/th06run.bat will setup PATH and other
+    # environment variables for the MSVC toolchain to work before calling ninja.
+    run_windows_program(
+        [str(SCRIPTS_DIR / "th06run.bat"), "ninja", "build/th06e.exe"],
+        cwd=str(SCRIPTS_DIR.parent),
+    )
+
+
 def main():
     parser = argparse.ArgumentParser("th06-build")
     parser.add_argument(
@@ -19,15 +31,8 @@ def main():
         build_type = BuildType.NORMAL
     elif args.build_type == "diffbuild":
         build_type = BuildType.DIFFBUILD
-    configure(build_type)
 
-    # Then, run the build. We use run_windows_program to automatically go through
-    # wine if running on linux/macos. scripts/th06run.bat will setup PATH and other
-    # environment variables for the MSVC toolchain to work before calling ninja.
-    run_windows_program(
-        [str(SCRIPTS_DIR / "th06run.bat"), "ninja", "build/th06e.exe"],
-        cwd=str(SCRIPTS_DIR.parent),
-    )
+    build(build_type)
 
 
 if __name__ == "__main__":
