1dc9dfd95a670ba3cc12ce5676fa60cb73fdb000
Mary Guillemard <mary@mary.zone>
2024-03-10 23:53:52 +0100

    Address roblabla's comments
    Signed-off-by: Mary Guillemard <mary@mary.zone>

--- src/ZunTimer.cpp
+++ src/ZunTimer.cpp
@@ -14,24 +14,24 @@ void ZunTimer::Increment(i32 value)
     if (g_Supervisor.framerateMultiplier > 0.99f)
     {
         this->current = this->current + value;
-    }
-    else
-    {
-        if (value < 0)
-        {
-            Decrement(-value);
-        }
-        else
-        {
-            this->previous = this->current;
-            this->subFrame = g_Supervisor.effectiveFramerateMultiplier * (float)value + this->subFrame;
 
-            while (this->subFrame >= 1.0f)
-            {
-                this->current++;
-                this->subFrame = this->subFrame - 1.0f;
-            }
-        }
+        return;
+    }
+
+    if (value < 0)
+    {
+        Decrement(-value);
+
+        return;
+    }
+
+    this->previous = this->current;
+    this->subFrame = g_Supervisor.effectiveFramerateMultiplier * (float)value + this->subFrame;
+
+    while (this->subFrame >= 1.0f)
+    {
+        this->current++;
+        this->subFrame = this->subFrame - 1.0f;
     }
 }
 
@@ -40,24 +40,24 @@ void ZunTimer::Decrement(i32 value)
     if (g_Supervisor.framerateMultiplier > 0.99f)
     {
         this->current = this->current - value;
-    }
-    else
-    {
-        if (value < 0)
-        {
-            Increment(-value);
-        }
-        else
-        {
-            this->previous = this->current;
-            this->subFrame = this->subFrame - g_Supervisor.effectiveFramerateMultiplier * (float)value;
 
-            while (this->subFrame < 0.0f)
-            {
-                this->current--;
-                this->subFrame = this->subFrame + 1.0f;
-            }
-        }
+        return;
+    }
+
+    if (value < 0)
+    {
+        Increment(-value);
+
+        return;
+    }
+
+    this->previous = this->current;
+    this->subFrame = this->subFrame - g_Supervisor.effectiveFramerateMultiplier * (float)value;
+
+    while (this->subFrame < 0.0f)
+    {
+        this->current--;
+        this->subFrame = this->subFrame + 1.0f;
     }
 }
 #pragma optimize("s", off)
