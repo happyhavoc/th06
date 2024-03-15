37b31137b0cf46afd9c0a08dd82492fa1962f2ee
Mary Guillemard <mary@mary.zone>
2024-03-16 00:56:27 +0100

    Add missing asserts for ZunVec2 and ZunVec3
    Signed-off-by: Mary Guillemard <mary@mary.zone>

--- src/ZunMath.hpp
+++ src/ZunMath.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include "inttypes.hpp"
+#include <Windows.h>
 
 struct ZunVec2
 {
     f32 x;
     f32 y;
 };
+C_ASSERT(sizeof(ZunVec2) == 0x8);
 
 struct ZunVec3
 {
@@ -13,3 +15,4 @@ struct ZunVec3
     f32 y;
     f32 z;
 };
+C_ASSERT(sizeof(ZunVec3) == 0xC);
