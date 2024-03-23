4a8f341aeddc18d41a03e945558b5b392f248db9
tymek <tymek24m@gmail.com>
2024-03-23 14:45:17 +0100

    `MainMenu` size is now correct and the dllbuild works correctly

--- src/MainMenu.hpp
+++ src/MainMenu.hpp
@@ -3,6 +3,7 @@
 #include <D3D8.h>
 
 #include "AnmVm.hpp"
+#include "Chain.hpp"
 #include "ZunResult.hpp"
 #include "inttypes.hpp"
 
@@ -35,7 +36,7 @@ struct MainMenu
 
     AnmVm vm[122];
     i32 cursor;
-    i8 padding[68];
+    i8 padding[0x40];
     u32 unk_81e4;
     i32 chosenReplay;
     i32 replayFilesNum;
@@ -47,5 +48,22 @@ struct MainMenu
     D3DCOLOR color2;
     D3DCOLOR color1;
     u32 unk_820c;
-    i8 padding2[0x8D24];
+    u32 isActive;
+    u32 wasActive;
+    i8 padding2[4];
+    i16 controlMapping[9];
+    i8 padding3[6];
+    ChainElem* chainCalc;
+    ChainElem* chainDraw;
+    char replayFilePaths[60][512];
+    char replayFileName[60][8];
+    i8 replayFileData[60][0x50];
+    i8* currentReplay;
+    i32* unk_10ee0;
+    f32* unk_10ee4;
+    i8 padding4[64];
+    u32 unk_10f28;
+    u32 unk_10f2c;
+    u32 time_related;
 };
+C_ASSERT(sizeof(MainMenu) == 0x10f34);
