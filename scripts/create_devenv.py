340d22ce1dafc00aa305f60bc9c4761f01674d44
roblabla <unfiltered@roblab.la>
2024-03-27 22:30:58 +0100

    Fix ruff errors

--- scripts/create_devenv.py
+++ scripts/create_devenv.py
@@ -257,9 +257,10 @@ def download_requirement_torrent(dl_cache_path, requirement, aria2c_path):
     try:
         os.rmdir(str(dl_cache_path / requirement["torrent_dirname"]))
         os.remove(str(dl_cache_path / requirement["torrent_dirname"] + ".torrent"))
-    except:
+    except Exception:
         print("Failed to remove torrent directory, should be removed manually.")
         pass
+
 
 def download_requirements(dl_cache_path, steps, should_torrent):
     requirements = [
