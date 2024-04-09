06372ba170864c81cf680d5ab80a7384efeadd01
Renzo Poggio <renzo.martin904@gmail.com>
2024-04-09 13:18:57 -0300

    Implement GameManager Constructor
    - Implements GameManager::GameManager
    - Fixes GameManager Struct offsets and adds all the ghidra server variables

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,3 +1,16 @@
 #include "GameManager.hpp"
 
 DIFFABLE_STATIC(GameManager, g_GameManager);
+
+#pragma optimize("s", on)
+GameManager::GameManager()
+{
+
+    memset(this, 0, sizeof(GameManager));
+
+    (this->arcadeRegionTopLeftPos).x = 32.0;
+    (this->arcadeRegionTopLeftPos).y = 16.0;
+    (this->arcadeRegionSize).x = 384.0;
+    (this->arcadeRegionSize).y = 448.0;
+}
+#pragma optimize("s", off)
