e63a06a29ac59546d278491d5c20c2b8426b7f5f
roblabla <unfiltered@roblab.la>
2024-05-17 20:12:04 +0200

    Introduce new color manipulation macros

--- src/ZunColor.hpp
+++ src/ZunColor.hpp
@@ -8,6 +8,14 @@
 #define COLOR_RED 0xffff0000
 #define COLOR_PINK 0xffffe0e0
 
+#define COLOR_RGB_MASK 0x00FFFFFF
+#define COLOR_ALPHA_MASK 0xFF000000
+#define COLOR_RGB(color) ((color) & COLOR_RGB_MASK)
+#define COLOR_ALPHA(color) (((color) & COLOR_ALPHA_MASK) >> 24)
+#define COLOR_SET_ALPHA(color, alpha) (((alpha) << 24) | COLOR_RGB(color))
+#define COLOR_SET_ALPHA2(color, alpha) (COLOR_RGB(color) | (((alpha) & 0xff) << 24))
+#define COLOR_COMBINE_ALPHA(color, alpha) (((alpha) & COLOR_ALPHA_MASK) | COLOR_RGB(color))
+
 #define COLOR_MENU_ACTIVE_BACKGROUND 0x40000000
 // TODO: find a better name for this color
 #define COLOR_START_MENU_ITEM_INACTIVE 0x80300000
