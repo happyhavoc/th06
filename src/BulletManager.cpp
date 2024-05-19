98e70920b5f6f428d2b8d3a2cf301dc6ccc7aff1
roblabla <unfiltered@roblab.la>
2024-05-19 13:26:52 +0200

    Implement BulletManager::RegisterChain

--- src/BulletManager.cpp
+++ src/BulletManager.cpp
@@ -1,3 +1,50 @@
 #include "BulletManager.hpp"
+#include "Chain.hpp"
+#include "ChainPriorities.hpp"
 
 DIFFABLE_STATIC(BulletManager, g_BulletManager);
+DIFFABLE_STATIC(ChainElem, g_BulletManagerCalcChain);
+DIFFABLE_STATIC(ChainElem, g_BulletManagerDrawChain);
+DIFFABLE_STATIC_ARRAY_ASSIGN(u32, 28, g_EffectsColorWithTextureBlending) = {
+    0xff000000, 0xff303030, 0xff606060, 0xff500000, 0xff900000, 0xffff2020, 0xff400040,
+    0xff800080, 0xffff30ff, 0xff000050, 0xff000090, 0xff2020ff, 0xff203060, 0xff304090,
+    0xff3080ff, 0xff005000, 0xff009000, 0xff20ff20, 0xff206000, 0xff409010, 0xff80ff20,
+    0xff505000, 0xff909000, 0xffffff20, 0xff603000, 0xff904010, 0xfff08020, 0xffffffff};
+
+DIFFABLE_STATIC_ARRAY_ASSIGN(u32, 28, g_EffectsColorWithoutTextureBlending) = {
+    0xfff0f0f0, 0xfff0f0f0, 0xffffffff, 0xffffe0e0, 0xffffe0e0, 0xffffe0e0, 0xffffe0ff,
+    0xffffe0ff, 0xffffe0ff, 0xffe0e0ff, 0xffe0e0ff, 0xffe0e0ff, 0xffe0ffff, 0xffe0ffff,
+    0xffe0ffff, 0xffe0ffe0, 0xffe0ffe0, 0xffe0ffe0, 0xffe0ffe0, 0xffe0ffe0, 0xffe0ffe0,
+    0xffffffe0, 0xffffffe0, 0xffffffe0, 0xffffe0e0, 0xffffe0e0, 0xffffe0e0, 0xffffffff};
+DIFFABLE_STATIC_ASSIGN(u32 *, g_EffectsColor) = g_EffectsColorWithTextureBlending;
+
+ZunResult BulletManager::RegisterChain(char *bulletAnmPath)
+{
+    BulletManager *mgr = &g_BulletManager;
+    if (((g_Supervisor.cfg.opts >> GCOS_USE_D3D_HW_TEXTURE_BLENDING) & 1) == 0)
+    {
+        g_EffectsColor = g_EffectsColorWithTextureBlending;
+    }
+    else
+    {
+        g_EffectsColor = g_EffectsColorWithoutTextureBlending;
+    }
+    mgr->InitializeToZero();
+    mgr->bulletAnmPath = bulletAnmPath;
+    g_BulletManagerCalcChain.callback = (ChainCallback)BulletManager::OnUpdate;
+    g_BulletManagerCalcChain.addedCallback = NULL;
+    g_BulletManagerCalcChain.deletedCallback = NULL;
+    g_BulletManagerCalcChain.addedCallback = (ChainAddedCallback)BulletManager::AddedCallback;
+    g_BulletManagerCalcChain.deletedCallback = (ChainDeletedCallback)BulletManager::DeletedCallback;
+    g_BulletManagerCalcChain.arg = mgr;
+    if (g_Chain.AddToCalcChain(&g_BulletManagerCalcChain, TH_CHAIN_PRIO_CALC_BULLETMANAGER) != ZUN_SUCCESS)
+    {
+        return ZUN_ERROR;
+    }
+    g_BulletManagerDrawChain.callback = (ChainCallback)BulletManager::OnDraw;
+    g_BulletManagerDrawChain.addedCallback = NULL;
+    g_BulletManagerDrawChain.deletedCallback = NULL;
+    g_BulletManagerDrawChain.arg = mgr;
+    g_Chain.AddToDrawChain(&g_BulletManagerDrawChain, TH_CHAIN_PRIO_DRAW_BULLETMANAGER);
+    return ZUN_SUCCESS;
+}
