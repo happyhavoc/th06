53d8b5b382f4bb5d6d4209c07c98ace5d2b78981
roblabla <unfiltered@roblab.la>
2024-09-14 15:18:23 +0200

    Implement GuiImpl::DrawDialogue

--- src/GuiImpl.cpp
+++ src/GuiImpl.cpp
@@ -5,6 +5,95 @@
 
 namespace th06
 {
+
+#pragma var_order(dialogueBoxHeight, vertices)
+#pragma optimize("s", on)
+ZunResult GuiImpl::DrawDialogue()
+{
+    f32 dialogueBoxHeight;
+
+    if (this->msg.currentMsgIdx < 0)
+    {
+        return ZUN_ERROR;
+    }
+    if (g_GameManager.currentStage == 6 && (this->msg.currentMsgIdx == 1 || this->msg.currentMsgIdx == 11))
+    {
+        return ZUN_SUCCESS;
+    }
+    if ((i32)(this->msg.timer.current < 60))
+    {
+        dialogueBoxHeight = this->msg.timer.AsFramesFloat() * 48.0f / 60.0f;
+    }
+    else
+    {
+        dialogueBoxHeight = 48.0f;
+    }
+    VertexDiffuseXyzrwh vertices[4];
+    // Probably not what Zun wrote, but I don't like Zun's design. My guess is
+    // Zun made a separate vertex structure with a D3DXVECTOR3 for the xyz, a
+    // separate f32 for the w, and a D3DCOLOR for the diffuse. This kinda makes
+    // no sense though - the position is a D3DXVECTOR4.
+    memcpy(&vertices[0].position,
+           &D3DXVECTOR3(g_GameManager.arcadeRegionTopLeftPos.x + (g_GameManager.arcadeRegionSize.x - 256.0f) / 2.0f -
+                            16.0f,
+                        384.0f, 0.0f),
+           sizeof(D3DXVECTOR3));
+
+    memcpy(&vertices[1].position,
+           &D3DXVECTOR3(g_GameManager.arcadeRegionTopLeftPos.x + (g_GameManager.arcadeRegionSize.x - 256.0f) / 2.0f +
+                            256.0f + 16.0f,
+                        384.0f, 0.0f),
+           sizeof(D3DXVECTOR3));
+
+    memcpy(&vertices[2].position,
+           &D3DXVECTOR3(g_GameManager.arcadeRegionTopLeftPos.x + (g_GameManager.arcadeRegionSize.x - 256.0f) / 2.0f -
+                            16.0f,
+                        384.0f + dialogueBoxHeight, 0.0f),
+           sizeof(D3DXVECTOR3));
+
+    memcpy(&vertices[3].position,
+           &D3DXVECTOR3(g_GameManager.arcadeRegionTopLeftPos.x + (g_GameManager.arcadeRegionSize.x - 256.0f) / 2.0f +
+                            256.0f + 16.0f,
+                        384.0f + dialogueBoxHeight, 0.0f),
+           sizeof(D3DXVECTOR3));
+
+    vertices[0].diffuse = vertices[1].diffuse = 0xd0000000;
+    vertices[2].diffuse = vertices[3].diffuse = 0x90000000;
+    vertices[0].position.w = vertices[1].position.w = vertices[2].position.w = vertices[3].position.w = 1.0f;
+    g_AnmManager->DrawNoRotation(&this->msg.portraits[0]);
+    g_AnmManager->DrawNoRotation(&this->msg.portraits[1]);
+    if (((g_Supervisor.cfg.opts >> GCOS_NO_COLOR_COMP) & 1) == 0)
+    {
+        g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_SELECTARG1);
+        g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_SELECTARG1);
+    }
+    g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_DIFFUSE);
+    g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_DIFFUSE);
+    if (((g_Supervisor.cfg.opts >> GCOS_TURN_OFF_DEPTH_TEST) & 1) == 0)
+    {
+        g_Supervisor.d3dDevice->SetRenderState(D3DRS_ZWRITEENABLE, 0);
+    }
+    g_Supervisor.d3dDevice->SetVertexShader(D3DFVF_DIFFUSE | D3DFVF_XYZRHW);
+    g_Supervisor.d3dDevice->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, vertices, sizeof(vertices[0]));
+    g_AnmManager->SetCurrentVertexShader(0xff);
+    g_AnmManager->SetCurrentColorOp(0xff);
+    g_AnmManager->SetCurrentBlendMode(0xff);
+    g_AnmManager->SetCurrentZWriteDisable(0xff);
+    if (((g_Supervisor.cfg.opts >> GCOS_NO_COLOR_COMP) & 1) == 0)
+    {
+        g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, 4);
+        g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_COLOROP, 4);
+    }
+    g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_ALPHAARG1, 2);
+    g_Supervisor.d3dDevice->SetTextureStageState(0, D3DTSS_COLORARG1, 2);
+    g_AnmManager->DrawNoRotation(&this->msg.dialogueLines[0]);
+    g_AnmManager->DrawNoRotation(&this->msg.dialogueLines[1]);
+    g_AnmManager->DrawNoRotation(&this->msg.introLines[0]);
+    g_AnmManager->DrawNoRotation(&this->msg.introLines[1]);
+    return ZUN_SUCCESS;
+}
+#pragma optimize("", on)
+
 #pragma optimize("s", on)
 void GuiImpl::MsgRead(i32 msgIdx)
 {
