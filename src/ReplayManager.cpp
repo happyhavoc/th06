2d166aeffbed07913e358be0c6d7bd827c64c892
roblabla <unfiltered@roblab.la>
2024-11-01 01:23:42 +0100

    Implement ReplayManager::RegisterChain

--- src/ReplayManager.cpp
+++ src/ReplayManager.cpp
@@ -1,6 +1,70 @@
 #include "ReplayManager.hpp"
+#include "Supervisor.hpp"
 
 namespace th06
 {
+DIFFABLE_STATIC(ReplayManager *, g_ReplayManager)
 
-};
+ZunResult ReplayManager::RegisterChain(i32 isDemo, char *replayFile)
+{
+    ReplayManager *replayMgr;
+
+    if (g_Supervisor.framerateMultiplier < 0.99f && !isDemo)
+    {
+        return ZUN_SUCCESS;
+    }
+    g_Supervisor.framerateMultiplier = 1.0f;
+    if (g_ReplayManager == NULL)
+    {
+        replayMgr = new ReplayManager();
+        g_ReplayManager = replayMgr;
+        replayMgr->replayData = NULL;
+        replayMgr->isDemo = isDemo;
+        replayMgr->replayFile = replayFile;
+        switch (isDemo)
+        {
+        case false:
+            replayMgr->calcChain = g_Chain.CreateElem((ChainCallback)ReplayManager::OnUpdate);
+            replayMgr->calcChain->addedCallback = (ChainAddedCallback)AddedCallback;
+            replayMgr->calcChain->deletedCallback = (ChainDeletedCallback)DeletedCallback;
+            replayMgr->drawChain = g_Chain.CreateElem((ChainCallback)ReplayManager::OnDraw);
+            replayMgr->calcChain->arg = replayMgr;
+            if (g_Chain.AddToCalcChain(replayMgr->calcChain, TH_CHAIN_PRIO_CALC_REPLAYMANAGER))
+            {
+                return ZUN_ERROR;
+            }
+            replayMgr->calcChainDemoHighPrio = NULL;
+            break;
+        case true:
+            replayMgr->calcChain = g_Chain.CreateElem((ChainCallback)ReplayManager::OnUpdateDemoHighPrio);
+            replayMgr->calcChain->addedCallback = (ChainAddedCallback)AddedCallbackDemo;
+            replayMgr->calcChain->deletedCallback = (ChainDeletedCallback)DeletedCallback;
+            replayMgr->drawChain = g_Chain.CreateElem((ChainCallback)ReplayManager::OnDraw);
+            replayMgr->calcChain->arg = replayMgr;
+            if (g_Chain.AddToCalcChain(replayMgr->calcChain, TH_CHAIN_PRIO_CALC_LOW_PRIO_REPLAYMANAGER_DEMO))
+            {
+                return ZUN_ERROR;
+            }
+            replayMgr->calcChainDemoHighPrio = g_Chain.CreateElem((ChainCallback)ReplayManager::OnUpdateDemoLowPrio);
+            replayMgr->calcChainDemoHighPrio->arg = replayMgr;
+            g_Chain.AddToCalcChain(replayMgr->calcChainDemoHighPrio, TH_CHAIN_PRIO_CALC_HIGH_PRIO_REPLAYMANAGER_DEMO);
+            break;
+        }
+        replayMgr->drawChain->arg = replayMgr;
+        g_Chain.AddToDrawChain(replayMgr->drawChain, TH_CHAIN_PRIO_DRAW_REPLAYMANAGER);
+    }
+    else
+    {
+        switch (isDemo)
+        {
+        case false:
+            AddedCallback(g_ReplayManager);
+            break;
+        case true:
+            return AddedCallbackDemo(g_ReplayManager);
+            break;
+        }
+    }
+    return ZUN_SUCCESS;
+}
+}; // namespace th06
--- src/ReplayManager.hpp
+++ src/ReplayManager.hpp
@@ -1,14 +1,40 @@
 #pragma once
 
+#include "Chain.hpp"
+#include "ChainPriorities.hpp"
 #include "ReplayData.hpp"
+#include "inttypes.hpp"
 
 namespace th06
 {
 struct ReplayManager
 {
     static ZunResult RegisterChain(i32 isDemo, char *replayFile);
+    static ChainCallbackResult OnUpdate(ReplayManager *mgr);
+    static ChainCallbackResult OnUpdateDemoHighPrio(ReplayManager *mgr);
+    static ChainCallbackResult OnUpdateDemoLowPrio(ReplayManager *mgr);
+    static ChainCallbackResult OnDraw(ReplayManager *mgr);
+    static ZunResult AddedCallback(ReplayManager *mgr);
+    static ZunResult AddedCallbackDemo(ReplayManager *mgr);
+    static ZunResult DeletedCallback(ReplayManager *mgr);
     static void StopRecording();
     static void SaveReplay(char *replay_path, char *param_2);
     static ZunResult ValidateReplayData(ReplayData *data, i32 fileSize);
+
+    ReplayManager()
+    {
+    }
+
+    u32 frameId;
+    ReplayData *replayData;
+    i32 isDemo;
+    char *replayFile;
+    u8 unk10[52];
+    u16 unk44;
+    ReplayDataInput *replayInputs;
+    ReplayDataInput *replayInputStageBookmarks[7];
+    ChainElem *calcChain;
+    ChainElem *drawChain;
+    ChainElem *calcChainDemoHighPrio;
 };
 }; // namespace th06
