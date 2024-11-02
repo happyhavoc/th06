7979be05a2ff838847e7ebc599aa43c2931c7d94
wearrrrr <contact@wearr.dev>
2024-11-02 14:35:38 -0500

    Address concerns from roblabla, static methods are now properly cdecl and mappings should now be accurate.

--- src/MusicRoom.cpp
+++ src/MusicRoom.cpp
@@ -2,7 +2,6 @@
 
 namespace th06 {
     DIFFABLE_STATIC(MusicRoom, g_MusicRoom);
-    DIFFABLE_STATIC(ChainElem, g_MusicRoomDrawChain);
     
     ZunResult MusicRoom::RegisterChain(void) {
 
--- src/MusicRoom.hpp
+++ src/MusicRoom.hpp
@@ -7,12 +7,12 @@ namespace th06
 {
 struct MusicRoom
 {
-    static ZunResult __stdcall AddedCallback(MusicRoom* musicRoom);
-    static ZunResult __stdcall DeletedCallback(MusicRoom* musicRoom);
-    void drawMusicList();
+    static ZunResult AddedCallback(MusicRoom* musicRoom);
+    static ZunResult DeletedCallback(MusicRoom* musicRoom);
+    void DrawMusicList();
     ZunResult FUN_00424e8f();
-    static ChainCallbackResult __stdcall OnDraw(MusicRoom* musicRoom);
-    static ChainCallbackResult __stdcall OnUpdate(MusicRoom* musicRoom);
+    static ChainCallbackResult OnDraw(MusicRoom* musicRoom);
+    static ChainCallbackResult OnUpdate(MusicRoom* musicRoom);
     static ZunResult RegisterChain();
 };
 }; // namespace th06
