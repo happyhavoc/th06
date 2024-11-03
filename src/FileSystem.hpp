08b3f9cca0058d7e459844878c205a673d70fb73
roblabla <unfiltered@roblab.la>
2024-11-03 02:36:12 +0100

    Remove two unused functions from FileSystem.hpp
    Those two functions aren't actually th06 functions, but the CRT _chdir
    and _mkdir functions.

--- src/FileSystem.hpp
+++ src/FileSystem.hpp
@@ -12,8 +12,6 @@ namespace FileSystem
 {
 u8 *OpenPath(char *filepath, int isExternalResource);
 int WriteDataToFile(char *path, void *data, size_t size);
-ZunResult CreateDirectoryInCWD(char *dir);
-void ChangeCWD(char *dir);
 } // namespace FileSystem
 DIFFABLE_EXTERN(u32, g_LastFileSize)
 }; // namespace th06
