4ec39b7b4a8a1872627d61517d14bd6c6185354f
sere <sere@example.com>
2024-11-06 18:31:17 +0400

    Implement MainMenu::SelectRelated

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -2363,5 +2363,50 @@ ZunResult MainMenu::LoadDiffCharSelect(MainMenu *menu)
 }
 #pragma optimize("", on)
 
+#pragma optimize("s", on)
+void th06::MainMenu::SelectRelated(MainMenu *menu, i16 btn_pressed, i16 old_mapping, ZunBool param_4)
+{
+    // I'm pretty sure this function handles going to the bottom when you press the
+    // bomb button or escape
+    if ((param_4 == 0) && (menu->controlMapping[0] == btn_pressed))
+    {
+        menu->controlMapping[0] = old_mapping;
+    }
+    if (menu->controlMapping[1] == btn_pressed)
+    {
+        menu->controlMapping[1] = old_mapping;
+    }
+    if ((param_4 == 0) && (menu->controlMapping[2] == btn_pressed))
+    {
+        menu->controlMapping[2] = old_mapping;
+    }
+    if (menu->controlMapping[4] == btn_pressed)
+    {
+        menu->controlMapping[4] = old_mapping;
+    }
+    if (menu->controlMapping[5] == btn_pressed)
+    {
+        menu->controlMapping[5] = old_mapping;
+    }
+    if (menu->controlMapping[6] == btn_pressed)
+    {
+        menu->controlMapping[6] = old_mapping;
+    }
+    if (menu->controlMapping[7] == btn_pressed)
+    {
+        menu->controlMapping[7] = old_mapping;
+    }
+    if (menu->controlMapping[3] == btn_pressed)
+    {
+        menu->controlMapping[3] = old_mapping;
+    }
+    if (menu->controlMapping[8] == btn_pressed)
+    {
+        menu->controlMapping[8] = old_mapping;
+    }
+    return;
+}
+#pragma optimize("", on)
+
 DIFFABLE_STATIC(MainMenu, g_MainMenu);
 }; // namespace th06
