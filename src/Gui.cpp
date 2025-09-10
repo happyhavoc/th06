07784a162423dd4c23516b405277c368b0e8c4b6
toadster172 <toadster172@gmail.com>
2025-09-10 01:10:27 -0500

    Replace hardcoded string literal with macro'd i18n.hpp version in RunMsg

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -625,7 +625,7 @@ ZunResult GuiImpl::RunMsg()
             this->songNameSprite.fontWidth = 16;
             this->songNameSprite.fontHeight = 16;
             AnmManager::DrawStringFormat(g_AnmManager, &this->songNameSprite, COLOR_RGB(COLOR_LIGHTCYAN),
-                                         COLOR_RGB(COLOR_BLACK), "♪%s",
+                                         COLOR_RGB(COLOR_BLACK), TH_SONG_NAME,
                                          g_Stage.stdData->songNames[this->msg.currentInstr->args.music]);
             if (g_Supervisor.PlayMidiFile(this->msg.currentInstr->args.music) != ZUN_SUCCESS)
             {
