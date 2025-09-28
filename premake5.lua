b7598ad43b4aea270ce19dea01353b38c7ec7de9
siohaza <siohaza@proton.me>
2025-09-28 20:06:03 +0300

    build: rewrite premake build

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,15 @@
 workspace "th06"
-configurations { "Debug", "Release" }
-location "build"
+  configurations { "Debug", "Release" }
+  location "build"
 
 project "th06"
-language "C++"
-cppdialect "C++20"
-targetname "th06"
-targetdir "."
-objdir "obj"
+  language "C++"
+  cppdialect "C++20"
+  targetname "th06"
+  targetdir "."
+  objdir "obj"
 
-files {
+  files {
     "src/AnmManager.cpp",
     "src/AsciiManager.cpp",
     "src/BombData.cpp",
@@ -46,74 +46,76 @@ files {
     "src/pbg3/IPbg3Parser.cpp",
     "src/pbg3/Pbg3Archive.cpp",
     "src/pbg3/Pbg3Parser.cpp",
-
     -- keep headers visible
     "src/**.hpp"
-}
-includedirs { "src" }
+  }
 
-filter "toolset:gcc"
-buildoptions { "-Wall", "-Wextra", "-Wpedantic", "-Wno-gnu-anonymous-struct" }
+  includedirs { "src" }
 
-filter "toolset:clang"
-buildoptions { "-Wall", "-Wextra", "-Wpedantic", "-Wno-gnu-anonymous-struct" }
+  filter "toolset:gcc"   buildoptions { "-Wall", "-Wextra", "-Wpedantic" }
+  filter "toolset:clang" buildoptions { "-Wall", "-Wextra", "-Wpedantic", "-Wno-gnu-anonymous-struct" }
+  filter {}
 
-filter { "system:not windows" }
-kind "ConsoleApp"
-local sdl2_cflags = os.outputof("sdl2-config --cflags") or ""
-local sdl2_libs   = os.outputof("sdl2-config --libs")   or ""
-if #sdl2_cflags > 0 then buildoptions { sdl2_cflags } end
+  kind "WindowedApp"
+
+  filter "system:linux"
+    local sdl2_cflags = os.outputof("sdl2-config --cflags") or ""
+    local sdl2_libs   = os.outputof("sdl2-config --libs")   or ""
+    if #sdl2_cflags > 0 then buildoptions { sdl2_cflags } end
     if #sdl2_libs   > 0 then linkoptions  { sdl2_libs }   end
-        links { "GL", "SDL2_image", "SDL2_ttf", "m" }
+    links { "GL", "SDL2_image", "SDL2_ttf", "m" }
+  filter {}
 
-        filter { "system:windows" }
-        kind "WindowedApp"
-        defines { "NOMINMAX", "WIN32_LEAN_AND_MEAN" }
+  filter "system:windows"
+    defines { "NOMINMAX", "WIN32_LEAN_AND_MEAN" }
+  filter {}
 
-        filter { "system:windows", "action:vs*" }
-        warnings "Extra"
-        links { "SDL2", "SDL2main", "SDL2_image", "SDL2_ttf", "opengl32" }
-        local SDL2_DIR       = os.getenv("SDL2_DIR")
-        local SDL2_IMAGE_DIR = os.getenv("SDL2_IMAGE_DIR")
-        local SDL2_TTF_DIR   = os.getenv("SDL2_TTF_DIR")
-        if SDL2_DIR then
-            includedirs { (SDL2_DIR .. "/include") }
-            filter { "system:windows", "action:vs*", "architecture:x86_64" }
-            libdirs { (SDL2_DIR .. "/lib/x64") }
-            filter { "system:windows", "action:vs*", "architecture:x86" }
-            libdirs { (SDL2_DIR .. "/lib/x86") }
-            filter {}
-            end
-            if SDL2_IMAGE_DIR then
-                includedirs { (SDL2_IMAGE_DIR .. "/include") }
-                filter { "system:windows", "action:vs*", "architecture:x86_64" }
-                libdirs { (SDL2_IMAGE_DIR .. "/lib/x64") }
-                filter { "system:windows", "action:vs*", "architecture:x86" }
-                libdirs { (SDL2_IMAGE_DIR .. "/lib/x86") }
-                filter {}
-                end
-                if SDL2_TTF_DIR then
-                    includedirs { (SDL2_TTF_DIR .. "/include") }
-                    filter { "system:windows", "action:vs*", "architecture:x86_64" }
-                    libdirs { (SDL2_TTF_DIR .. "/lib/x64") }
-                    filter { "system:windows", "action:vs*", "architecture:x86" }
-                    libdirs { (SDL2_TTF_DIR .. "/lib/x86") }
-                    filter {}
-                    end
+  filter { "system:windows", "action:vs*" }
+    warnings "Extra"
+    links { "SDL2", "SDL2main", "SDL2_image", "SDL2_ttf", "opengl32" }
 
-                    filter { "system:windows", "action:not vs*" }
-                    links { "opengl32" }
-                    local pc_cflags = os.outputof("pkg-config --cflags sdl2 SDL2_image SDL2_ttf") or ""
-                    local pc_libs   = os.outputof("pkg-config --libs   sdl2 SDL2_image SDL2_ttf") or ""
-                    if #pc_cflags > 0 then buildoptions { pc_cflags } end
-                        if #pc_libs   > 0 then linkoptions  { pc_libs }   end
+    local SDL2_DIR       = os.getenv("SDL2_DIR")
+    local SDL2_IMAGE_DIR = os.getenv("SDL2_IMAGE_DIR")
+    local SDL2_TTF_DIR   = os.getenv("SDL2_TTF_DIR")
 
-                            filter "configurations:Debug"
-                            defines { "DEBUG" }
-                            symbols "On"
-                            optimize "Off"
+    if SDL2_DIR then
+      includedirs { SDL2_DIR .. "/include" }
+      filter { "architecture:x86_64" } libdirs { SDL2_DIR .. "/lib/x64" }
+      filter { "architecture:x86"    } libdirs { SDL2_DIR .. "/lib/x86" }
+      filter {}
+    end
 
-                            filter "configurations:Release"
-                            defines { "NDEBUG" }
-                            optimize "Speed"
-                            symbols "Off"
+    if SDL2_IMAGE_DIR then
+      includedirs { SDL2_IMAGE_DIR .. "/include" }
+      filter { "architecture:x86_64" } libdirs { SDL2_IMAGE_DIR .. "/lib/x64" }
+      filter { "architecture:x86"    } libdirs { SDL2_IMAGE_DIR .. "/lib/x86" }
+      filter {}
+    end
+
+    if SDL2_TTF_DIR then
+      includedirs { SDL2_TTF_DIR .. "/include" }
+      filter { "architecture:x86_64" } libdirs { SDL2_TTF_DIR .. "/lib/x64" }
+      filter { "architecture:x86"    } libdirs { SDL2_TTF_DIR .. "/lib/x86" }
+      filter {}
+    end
+  filter {}
+
+  filter { "system:windows", "action:not vs*" }
+    links { "opengl32" }
+    local pc_cflags = os.outputof("pkg-config --cflags sdl2 SDL2_image SDL2_ttf") or ""
+    local pc_libs   = os.outputof("pkg-config --libs   sdl2 SDL2_image SDL2_ttf") or ""
+    if #pc_cflags > 0 then buildoptions { pc_cflags } end
+    if #pc_libs   > 0 then linkoptions  { pc_libs }   end
+  filter {}
+
+  filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+    optimize "Off"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "Speed"
+    symbols "Off"
+
+  filter {}
