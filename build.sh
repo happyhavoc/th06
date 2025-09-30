e977a44841131b25926341ec75200a2c5b2258d5
wearrrrr <contact@wearr.dev>
2025-09-30 08:10:03 -0500

    add a --clean parameter because premake5 doesn't have it yet.

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
-premake5 gmake
+CLEAN=false
+for arg in "$@"; do
+    if [ "$arg" = "--clean" ]; then
+        CLEAN=TRUE
+    fi
+done;
+
+if [ "$CLEAN" = "TRUE" ]; then
+    rm -rf build
+    rm -rf obj/
+    rm th06
+fi
+
+premake5 --cc=clang gmake
 cd build
 make -j${nproc} && cd ..
